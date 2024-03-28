f8f371d114bf8f48154ae7479924f4a19dad29d4
steve <ustinsteve@gmail.com>
2024-03-28 19:04:23 +0100

    identify file with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 #using Puppet to make changes to our configuration file
 
-file_line { 'identify the file':
+file_line{'identify the file':
 path => '/etc/ssh/ssh_config',
 line => '~/.ssh/school',
 }
 
-file_line { 'turn off passwd':
+file_line{'turn off passwd':
 path => '/etc/ssh/ssh_config',
 line => 'PasswordAuthentication no',
 }
