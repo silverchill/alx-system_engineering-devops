bc77aaf31a3d9eaaab38db71841d060e3412e9ed
steve <ustinsteve@gmail.com>
2024-04-25 14:57:20 +0100

    alphabetical order

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,8 +2,8 @@
 """script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress"""
 
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     num_ = argv[1]
