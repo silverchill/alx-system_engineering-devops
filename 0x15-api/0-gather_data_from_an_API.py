4494085ec9edacdef0697dbb1377cbb38d443b89
steve <ustinsteve@gmail.com>
2024-04-25 14:28:56 +0100

    edited

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,18 +11,17 @@ if __name__ == "__main__":
     user = requests.get(user_url)
     todo = requests.get(todo_url)
     
-    todo_l = todo.json()
-    user_l = user.json()['name']
-    ini_length = len(user_l)
+    todo_list = todo.json()
+    user_list = user.json()['name']
+    ini_length = len(user_list)
     length = 0
 
-    for task in todo_l:
+    for task in todo_list:
         if task['completed'] is True:
             length += 1
 
-    print("Employee {} is done with tasks"({}/{}):"
-          .format(user_l, length, ini_length))
-    for task in todo_l:
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_list, length, ini_length))
+    for task in todo_list:
         if task['completed'] is True:
             print('\t {}'.format(task['title']))
-            
